446b6efc3868a704bfaa3e262c30a325d0805072
kacenka7 <157618583+kacenka7@users.noreply.github.com>
2024-03-27 19:16:31 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+
+const elForm = document.querySelector("#registrace")
+
+elForm.addEventListener("input", (event) => {
+
+    const pole = document.querySelector(".field")
+    const email = document.querySelector("#email")
+
+
+    if(event.target.value.length > 1 || (email.value.includes("@"))) {
+    pole.classList.remove("ramecek")
+    
+    }
+    else {
+        pole.classList.add("ramecek")
+    }
+
+})
+
+elForm.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    const email = document.querySelector("#email")
+    elForm.textContent = `Hříšná duše ${email.value} byla zaregistrována`
+
+})
+
+
+
